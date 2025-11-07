77b0bf2ad22aaacff676bfa09ec7257828dbe0c3
badiwidya <adiwidya.berton13@gmail.com>
2025-11-07 22:08:57 +0700

    feat(list): implement list subcommand
    implementing `list` subcommand with 3 arguments:
    1. theme (default)
    2. icon
    3. cursor

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var ErrUsage = errors.New("")
+
+func main() {
+	if err := run(); err != nil {
+		if !errors.Is(err, ErrUsage) {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		printMainHelp(os.Stderr)
+		return ErrUsage
+	}
+
+	cmd := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	var err error
+	switch cmd {
+	case "help":
+		err = help(cmdArgs)
+	case "list":
+		err = list(cmdArgs)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
